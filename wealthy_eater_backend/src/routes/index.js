0a37671618c99721efaf9bc129b2b14f2babab68
Trần Phan Trung Kiên <148049256+Krezzy88@users.noreply.github.com>
2026-06-07 22:22:51 +0700

    Update index.js

--- wealthy_eater_backend/src/routes/index.js
+++ wealthy_eater_backend/src/routes/index.js
@@ -7,23 +7,14 @@ const recipeRoute = require('./user.recipe.route');
 const ingredientRoute = require('./ingredient.management.routes');
 const shoppingListRoute = require('./shopping_list.route');
 const micronutrientRoute = require('./micronutrient.management.routes');
-const recipeRoute = require('./recipe.route');
 const profileRoute = require('./profile.route');
-const ingredientRoute = require('./ingredient.management.routes');
-const shoppingListRoute = require('./shopping_list.route');
-const micronutrientRoute = require('./micronutrient.management.routes');
 
 router.use('/api/auth', authRoute);
 router.use('/api/user/recipes', recipeRoute);
 router.use('/api/ingredients', ingredientRoute);
 router.use('/api/user/shopping-list', shoppingListRoute);
 router.use('/api/micronutrients', micronutrientRoute);
-
-router.use('/api/recipes', recipeRoute);
 router.use('/api/profile', profileRoute);
-router.use('/api/ingredients', ingredientRoute);
-router.use('/api/user/shopping-list', shoppingListRoute);
-router.use('/api/micronutrients', micronutrientRoute);
 
 
 module.exports = router;
